3ec3034fd76e81cc12e7beefb18aa38010d7cf03
Mayowa Aj <mayowaaj69@hotmail.co.uk>
2017-06-23 10:12:58 +0100

    File Organisation

--- js/functions.js
+++ js/functions.js
@@ -2,12 +2,13 @@ window.addEventListener("load", function(){
   var banner = document.getElementById("banner").style;
     var header = document.getElementById("header").style;
   banner.opacity = "0.6";
-  banner.transform = "translateX(0%)";
   var footer = document.getElementById("footer").style;
-  footer.transform = "translateY(-100%)";
-  //header.transform = "translateY(-100%)";
-  window.onscroll = function(){
+ footer.transform = "translateY(-100%)";
+ setTimeout(function(){
+   header.transform = "translateY(-100%)";
+ }, 900);
 
+  window.onscroll = function(){
   }
   console.log(scrollY);
 });
